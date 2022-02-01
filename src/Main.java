0d407cbaccc5d221be5d60b5f95624334448010c
Alexei Baranov <baranov.alexalex@gmail.com>
2022-02-01 19:04:05 +0700

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Number one plus first branch");
+        System.out.println("Number three");
         System.out.println("Number two");
     }
 }
